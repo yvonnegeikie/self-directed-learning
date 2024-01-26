6a7202f4b558f7cb0dba0cec3d7a63bfbdc6fdc0
harmonious-parrot-66629 <153278035+harmonious-parrot-66629@users.noreply.github.com>
2024-01-26 07:52:13 +0000

    steps 1- 13 complete

---
+++ javascript/object/script.js
@@ -0,0 +1,35 @@
+let spaceship = {
+    passengers: [{ name: 'Space Dog' }],
+    telescope: {
+        yearBuilt: 2018,
+        model: "91031-XLT",
+        focalLength: 2032
+    },
+    crew: {
+        captain: {
+            name: 'Sandra',
+            degree: 'Computer Engineering',
+            encourageTeam() { console.log('We got this!') },
+            'favorite foods': ['cookies', 'cakes', 'candy', 'spinach']
+        }
+    },
+    engine: {
+        model: "Nimbus2000"
+    },
+    nanoelectronics: {
+        computer: {
+            terabytes: 100,
+            monitors: "HD"
+        },
+        'back-up': {
+            battery: "Lithium",
+            terabytes: 50
+        }
+    }
+};
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+
+let firstPassenger = spaceship.passengers[0];
+/*1.
+Create a variable capFave and assign the captain‘s favorite food (the element in the 0th index of her 'favorite foods' array) to it. Make sure to use bracket and dot notation to get the value of the food through nested access (don’t just copy the value into the variable!)*/
---
+++ javascript/project-scissors-paper-rock/script.js
@@ -0,0 +1,67 @@
+console.log('hi');
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return (userInput);
+    }
+    else {
+        console.log("Error!");
+    }
+};
+
+const getComputerChoice = randomNumber => {
+    Math.random(Math.floor() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+    if
+        (userChoice === computerChoice) {
+        return ("Tie!");
+    };
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return "Conputer wins!";
+        }
+        else {
+            return "Human wins!";
+        }
+    };
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return "Computer wins!";
+        }
+        else {
+            return "Human wins!";
+        }
+    };
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return "Computer wins!";
+        }
+        else {
+            return "Human wins!";
+        }
+    }
+};
+console.log(determineWinner('paper', 'scissors'));
+console.log(determineWinner('paper', 'paper'));
+console.log(determineWinner('paper', 'rock'));
+
+function playGame() {
+    const userChoice = getUserChoice('rock', 'paper', 'scissors');
+    const computerChoice = getComputerChoice();
+    console.log(playGame);
+};
+
