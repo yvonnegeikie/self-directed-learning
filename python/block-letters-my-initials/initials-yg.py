b38fea446b8c7746b3181a46fe575f82d29cbedb
harmonious-parrot-66629 <153278035+harmonious-parrot-66629@users.noreply.github.com>
2024-03-12 21:32:28 +0000

    python mini-projects

---
+++ python/block-letters-my-initials/initials-yg.py
@@ -0,0 +1,41 @@
+# Yvonne Geikie
+# Fun fact: I like to snowboard 
+firstName = """
+Y   Y  
+ Y Y
+  Y 
+  Y
+  Y
+  Y
+  """
+print(firstName)
+
+lastName = """
+ GGG 
+G   G
+G
+GGGGG
+G   G
+ GGG 
+  """
+print(lastName)
+
+fullName = """
+Y   Y   GGG
+ Y Y   G   G
+  Y    G
+  Y    GGGGG
+  Y    G   G
+  Y     GGG
+  """
+print(fullName)
+
+print("Y   Y   GGG")
+print(" Y Y   G   G")
+print("  Y    G")
+print("  Y    GGGGG")
+print("  Y    G   G")
+print("  Y    G   G")
+print("  Y     GGG ")
+
+
