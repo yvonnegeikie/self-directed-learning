f9bd51837deaa3c98319d75d8e1b47f46a273af3
harmonious-parrot-66629 <153278035+harmonious-parrot-66629@users.noreply.github.com>
2024-04-23 18:08:26 +0100

    add completed droid project

--- java/projetcs/Droid/Droid.java
+++ java/projetcs/Droid/Droid.java
@@ -7,10 +7,25 @@ public class Droid {
         // Prints Hello, I’m the droid: Codey
     }
 
+    // Define performTask() method
+    public String performTask(String task) {
+        return name + " is performing task: " + task;
+    }
+
     // Constructor method for Droid
     public Droid(String droidName, int batteryLevel) {
         name = droidName;
         batteryLevel = 100;
+    }
+
+    // Battery level drained from performing task
+    public void drainBattery() {
+        batteryLevel = batteryLevel - 10;
+    }
+
+    // New method: energyReport()
+    public void energyReport() {
+        System.out.println("Battery level: " + batteryLevel);
     }
 
     public static void main(String[] args) {
@@ -18,5 +33,14 @@ public class Droid {
 
         System.out.println(Codey);
         // Prints Droid@2aae9190
+
+        System.out.println(Codey.performTask("dancing"));
+        // Prints Codey is performing task: dancing
+
+        System.out.println("Battery level: " + Codey.batteryLevel);
+        // Prints Battery level: 0
+
+        // Call the energyReport() method
+        Codey.energyReport();
     }
 }
