d43c27022ddc2ef6915ec53b32dd8b7ceb1e8d1b
harmonious-parrot-66629 <153278035+harmonious-parrot-66629@users.noreply.github.com>
2024-04-23 15:37:13 +0100

    add main and car file complete

--- java/oop/Car.java
+++ java/oop/Car.java
@@ -5,19 +5,16 @@ package java.oop;
 public class Car {
     String make = "Holden";
     String model = "Commodore";
-    int year = 1998;
+    int year = 1994;
     String color = "white";
     double price = 1000.00;
-}
 
-// Method for drive 
-// Actions which the Car object can perform 
-void drive(){
-    System.out.println("You drive the car");
-}
+    // Method for drive
+    void drive() {
+        System.out.println("You drive the car");
+    }
 
-// Method for brake
-void brake(){
-
-    System.out.println("You step on the brakes");
-}
+    // Method for brake
+    void brake() {
+		System.out.println("You step on the brakes");
+    }
--- java/oop/Main.java
+++ java/oop/Main.java
@@ -3,17 +3,15 @@ package java.oop;
 public class Main {
     public static void main(String[] args) {
         // insert name of the class, Car, to create object
-        // myCar1 and myCar2 will have same attributes listed n Car class 
+        // myCar1 and myCar2 will have same attributes listed in Car class
         Car myCar1 = new Car();
         Car myCar2 = new Car();
-        // Reuse the Car class to create a second Car object 
+        // Reuse the Car class to create a second Car object
 
         System.out.println(myCar1.make);
         System.out.println(myCar1.model);
         System.out.println();
         System.out.println(myCar2.make);
         System.out.println(myCar2.model);
-  
-        
     }
 }
